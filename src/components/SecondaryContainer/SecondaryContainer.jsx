b01afe339d0ae61ad1531c5f62491fbef1df87af
Shobhit Nautiyal <shobhit.nautiyal99@gmail.com>
2024-07-30 12:36:24 +0530

    optional chaining

--- src/components/SecondaryContainer/SecondaryContainer.jsx
+++ src/components/SecondaryContainer/SecondaryContainer.jsx
@@ -3,8 +3,8 @@ import MovieList from "../SecondaryContainer/MovieList";
 import SeriesList from "../SecondaryContainer/SeriesList";
 
 const SecondaryContainer = () => {
-  const movies = useSelector((store) => store.movies);
-  const series = useSelector((store) => store.series);
+  const movies = useSelector((store) => store?.movies);
+  const series = useSelector((store) => store?.series);
 
   return (
     movies?.nowPlayingMovies && (
@@ -12,16 +12,22 @@ const SecondaryContainer = () => {
         <div className="mt-96 relative z-40 md:-mt-52 lg:-mt-52">
           <MovieList
             title={"Now Playing Movies"}
-            movies={movies.nowPlayingMovies}
+            movies={movies?.nowPlayingMovies}
           />
-          <SeriesList title={"Airing Today TV"} series={series.airingTodayTV} />
-          <MovieList title={"Trending Movies"} movies={movies.trendingMovies} />
-          <SeriesList title={"Trending TV"} series={series.trendingTV} />
+          <SeriesList
+            title={"Airing Today TV"}
+            series={series?.airingTodayTV}
+          />
+          <MovieList
+            title={"Trending Movies"}
+            movies={movies?.trendingMovies}
+          />
+          <SeriesList title={"Trending TV"} series={series?.trendingTV} />
           <MovieList
             title={"Top Rated Movies"}
-            movies={movies.topRatedMovies}
+            movies={movies?.topRatedMovies}
           />
-          <SeriesList title={"Top Rated TV"} series={series.topRatedTV} />
+          <SeriesList title={"Top Rated TV"} series={series?.topRatedTV} />
         </div>
       </div>
     )
